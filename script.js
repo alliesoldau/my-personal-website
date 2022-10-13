b465c932cc5353d2bad051ccc1003013f7093d71
Allie Soldau <alliesoldau@gmail.com>
2022-10-13 16:51:20 -0400

    Began work on getting the buttons to toggle. All can change color back and forth when individually pushed, but I need to figure out how to get them to turn off when the other buttons are pressed

--- script.js
+++ script.js
@@ -1,65 +1,94 @@
 
-// Focusing on the AboutMe bubble for now
-// Need to make the colors of the button toggle with each click
-// Need to make the entire AboutMe box toggle with each click
-// Need to add in this code for the other bubbles
 // Need to make it so clicking one button makes all the others revert to neutral
+// All buttons need to be clicked twice to start --> the JS is "changing" the color of the dots on the first click
+// To the yellow color. But it's meant to start out with the yellow color. So something is messed out with the logic.
 
 var element = document.getElementById('toggleAboutMe');
-element.onclick = function () { 
+element.onclick = function () {
+    var toggleResume = document.getElementById("toggleResume");
     if (this.style.backgroundColor == "rgb(224, 255, 79)") {
-        // Add double equals to all the others
-        // Convert HEX to RGB values
-        this.style.backgroundColor = "#57a773ff";
+        this.style.backgroundColor = "rgb(87, 167, 115)";
         this.style.borderRadius = "50% 50% 0% 0%";
         var boxAboutMe = document.getElementById("boxAboutMe");
-        boxAboutMe.style.display = "none";
+        boxAboutMe.style.display = "block";
+        // I'm playing with adding in this if statement to see if I can make the Resume dot change back to yellow and hide the block when I click the About Me Block. Once I get this to work
+// I'll extend it to the other blocks and buttons.  
+        if (toggleResume.style.backgroundColor == "rgb(87, 167, 115)") {
+            console.log(`RESUME NOW: ${toggleResume.style.backgroundColor}`);
+            //Currently it won't change the color (see log to show in action)
+            toggleResume.style.backgroundColor == "rgb(224, 255, 79)";
+            console.log(`RESUME NOW POST CHANGE: ${toggleResume.style.backgroundColor}`);
+            toggleResume.style.borderRadius = "50%";
+            }
     } else {
-        this.style.backgroundColor = "#e0ff4fff";
+        this.style.backgroundColor = "rgb(224, 255, 79)";
         var boxAboutMe = document.getElementById("boxAboutMe");
         this.style.borderRadius = "50%";
-        boxAboutMe.style.display = "block";
+        boxAboutMe.style.display = "none";
     } 
-    console.log(this.style.backgroundColor);
+    console.log(`About Me: ${this.style.backgroundColor}`);
  };
 
 var element = document.getElementById('toggleResume');
 element.onclick = function () { 
-    if (this.style.backgroundColor = "e0ff4fff") {
-        this.style.backgroundColor = "#57a773ff";
+    if (this.style.backgroundColor == "rgb(224, 255, 79)") {
+        this.style.backgroundColor = "rgb(87, 167, 115)";
         this.style.borderRadius = "50% 50% 0% 0%";
+        var boxResume = document.getElementById("boxResume");
+        boxResume.style.display = "block";
     } else {
-        this.style.backgroundColor = "#e0ff4fff";
+        this.style.backgroundColor = "rgb(224, 255, 79)";
+        var boxResume = document.getElementById("boxResume");
+        this.style.borderRadius = "50%";
+        boxResume.style.display = "none";
     } 
-};
+    console.log(`Resume: ${this.style.backgroundColor}`);
+ };
 
 var element = document.getElementById('toggleVolunteering');
 element.onclick = function () { 
-    if (this.style.backgroundColor = "e0ff4fff") {
-        this.style.backgroundColor = "#57a773ff";
+    if (this.style.backgroundColor == "rgb(224, 255, 79)") {
+        this.style.backgroundColor = "rgb(87, 167, 115)";
         this.style.borderRadius = "50% 50% 0% 0%";
-
+        var boxVolunteering = document.getElementById("boxVolunteering");
+        boxVolunteering.style.display = "block";
     } else {
-        this.style.backgroundColor = "#e0ff4fff";
+        this.style.backgroundColor = "rgb(224, 255, 79)";
+        var boxVolunteering = document.getElementById("boxVolunteering");
+        this.style.borderRadius = "50%";
+        boxVolunteering.style.display = "none";
     } 
-};
+    console.log(`Volunteering: ${this.style.backgroundColor}`);
+ };
 
 var element = document.getElementById('togglePassions');
 element.onclick = function () { 
-    if (this.style.backgroundColor = "e0ff4fff") {
-        this.style.backgroundColor = "#57a773ff";
+    if (this.style.backgroundColor == "rgb(224, 255, 79)") {
+        this.style.backgroundColor = "rgb(87, 167, 115)";
         this.style.borderRadius = "50% 50% 0% 0%";
+        var boxPassions = document.getElementById("boxPassions");
+        boxPassions.style.display = "block";
     } else {
-        this.style.backgroundColor = "#e0ff4fff";
+        this.style.backgroundColor = "rgb(224, 255, 79)";
+        var boxPassions = document.getElementById("boxPassions");
+        this.style.borderRadius = "50%";
+        boxPassions.style.display = "none";
     } 
+    console.log(`Passions: ${this.style.backgroundColor}`);
  };
 
 var element = document.getElementById('togglePersonalProjects');
 element.onclick = function () { 
-    if (this.style.backgroundColor = "e0ff4fff") {
-        this.style.backgroundColor = "#57a773ff";
+    if (this.style.backgroundColor == "rgb(224, 255, 79)") {
+        this.style.backgroundColor = "rgb(87, 167, 115)";
         this.style.borderRadius = "50% 50% 0% 0%";
+        var boxPersonalProjects = document.getElementById("boxPersonalProjects");
+        boxPersonalProjects.style.display = "block";
     } else {
-        this.style.backgroundColor = "#e0ff4fff";
+        this.style.backgroundColor = "rgb(224, 255, 79)";
+        var boxPersonalProjects = document.getElementById("boxPersonalProjects");
+        this.style.borderRadius = "50%";
+        boxPersonalProjects.style.display = "none";
     } 
+    console.log(`Personal Projects: ${this.style.backgroundColor}`);
  };
